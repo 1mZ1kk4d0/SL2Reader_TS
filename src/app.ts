7dffab2c4f13dc21017ee7712d7f8a16acd5b450
Michał Gębicki <59212335+mi5hmash@users.noreply.github.com>
2024-08-01 21:58:41 +0200

    my vision

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,22 @@ import fs from 'fs';
 
 const profile = {
     er: {
-        slot_data_offset: 6484,
-        slot_length: 588
+        is_encrypted: false,
+        character_name_max_length: 16,
+        character_slots_count: 10,
+        file_index: 10,
+        slot_data_offset: 6494,
+        slot_length: 588,
+        slots_occupancy_offset: 6484
     },
     ds3: {
-        slot_data_offset: 4244,
-        slot_length: 554
+        is_encrypted: true,
+        character_name_max_length: 16,
+        character_slots_count: 10,
+        file_index: 10,
+        slot_data_offset: 4254,
+        slot_length: 554,
+        slots_occupancy_offset: 4244
     }
 } as const;
 
@@ -19,7 +29,6 @@ class BND4Entry {
 
     //Declations
     profile: Profile;
-    index: number;
     raw: Buffer;
     size: number;
     charEntryOffset: number;
@@ -32,18 +41,18 @@ class BND4Entry {
     charEntryDataOffset: number;
     charEntryNameOffset: number;
 
-    constructor(raw: Buffer, index: number, profile: Profile, encrypted: boolean) {
+    constructor(raw: Buffer, profileKey: Profile) {
 
-
-        this.profile = profile;
         this.raw = raw;
-        this.index = index;
+        this.profile = profileKey;
+        
         this.size = raw.length;
 
         const entryHeaderOffset = 64;
         const entryHeaderLength = 32;
+        const profileData = profile[profileKey];
 
-        this.charEntryOffset = entryHeaderOffset + entryHeaderLength * this.index;
+        this.charEntryOffset = entryHeaderOffset + entryHeaderLength * profileData.file_index;
         this.charEntrySize = raw.readUInt32LE(this.charEntryOffset + 8);
         this.charEntryDataOffset = raw.readUInt32LE(this.charEntryOffset + 16);
         this.charEntryNameOffset = raw.readUInt32LE(this.charEntryOffset + 20);
@@ -52,7 +61,7 @@ class BND4Entry {
         this.iv = this.raw.subarray(this.charEntryDataOffset, this.charEntryDataOffset + 16);
         this.entryData = this.raw.subarray(this.charEntryDataOffset + 16, this.charEntryDataOffset + this.charEntrySize);
 
-        this.decryptedData = !encrypted ? Buffer.alloc(0) : decryptFile(this.entryData, this.iv);
+        this.decryptedData = !profileData.is_encrypted ? Buffer.alloc(0) : decryptFile(this.entryData, this.iv);
         this.checksum = this.raw.subarray(this.charEntryOffset, this.charEntryOffset + 16);
     }
 
@@ -69,7 +78,6 @@ class BND4Entry {
 
         const slotBytes = entry.subarray(profiles.slot_data_offset, profiles.slot_data_offset + maxCharacterSlotCount);
         
-   
         const slotOccupancy = [];
 
         for (let i = 0; i < 10; i++) {
@@ -105,37 +113,56 @@ class BND4Entry {
         console.log(this.getOccupancySlots())
 
         if (this.profile === "ds3") {
-            for (let i = 0; i <= this.index; i++) {
-                const offset_name = profile[this.profile].slot_data_offset + 30 + i * profile[this.profile].slot_length;
-                const offset_level = profile[this.profile].slot_data_offset + 30 + 34 + i * profile[this.profile].slot_length;
-                const offset3_timestamp = profile[this.profile].slot_data_offset + 30 + 38 + i * profile[this.profile].slot_length;
-                chars.push({
-                    character_name: readNullTerminatedUTF16LEString(this.decryptedData, offset_name),
-                    character_level: readInt(this.decryptedData, offset_level),
-                    character_time: readInt(this.decryptedData, offset3_timestamp),
-                    character_time_format: formatTime(readInt(this.decryptedData, offset3_timestamp))
-                });
+            const current_profile = profile[this.profile];
+            const name_section_size = current_profile.character_name_max_length * 2 + 2;
+            const checksum_with_padding_size = 16 + 4;
+            const level_size = 4;
+            for (let i = 0; i < current_profile.character_slots_count; i++) {
+                const offset_name = current_profile.slot_data_offset + checksum_with_padding_size + i * current_profile.slot_length;
+                const offset_level = current_profile.slot_data_offset + checksum_with_padding_size + name_section_size + i * current_profile.slot_length;
+                const offset3_timestamp = current_profile.slot_data_offset + checksum_with_padding_size + name_section_size + level_size + i * current_profile.slot_length;
+                const slot_unoccupied = this.decryptedData[current_profile.slots_occupancy_offset + checksum_with_padding_size + i] === 0;
+                if (slot_unoccupied) {
+                    chars.push({
+                        character_slot: i + 1
+                    });
+                } else {
+                    chars.push({
+                        character_slot: i + 1,
+                        character_name: readNullTerminatedUTF16LEString(this.decryptedData, offset_name),
+                        character_level: readInt(this.decryptedData, offset_level),
+                        character_time: readInt(this.decryptedData, offset3_timestamp),
+                        character_time_format: formatTime(readInt(this.decryptedData, offset3_timestamp))
+                    });
+                }
+
             }
         }
 
         if(this.profile === "er") {
-
-            for (let i = 0; i <= this.index; i++) {
-
-                const offset_name = profile[this.profile].slot_data_offset + 10 + i * profile[this.profile].slot_length;
-                const offset_level = profile[this.profile].slot_data_offset + 10 + 34 + i * profile[this.profile].slot_length;
-                const offset3_timestamp = profile[this.profile].slot_data_offset + 10 + 38 + i * profile[this.profile].slot_length;
-    
-                chars.push({
-                    character_name: readNullTerminatedUTF16LEString(this.entryData, offset_name),
-                    character_level: readInt(this.entryData, offset_level),
-                    character_time: readInt(this.entryData, offset3_timestamp),
-                    character_time_format: formatTime(readInt(this.entryData, offset3_timestamp))
-                });
+            const current_profile = profile[this.profile];
+            const name_section_size = current_profile.character_name_max_length * 2 + 2;
+            const level_size = 4;
+            for (let i = 0; i < current_profile.character_slots_count; i++) {
+                const offset_name = current_profile.slot_data_offset + i * current_profile.slot_length;
+                const offset_level = current_profile.slot_data_offset + name_section_size + i * current_profile.slot_length;
+                const offset3_timestamp = current_profile.slot_data_offset + name_section_size + level_size + i * current_profile.slot_length;
+                const slot_unoccupied = this.entryData[current_profile.slots_occupancy_offset + i] === 0;
+                if (slot_unoccupied) {
+                    chars.push({
+                        character_slot: i + 1
+                    });
+                } else {
+                    chars.push({
+                        character_slot: i + 1,
+                        character_name: readNullTerminatedUTF16LEString(this.entryData, offset_name),
+                        character_level: readInt(this.entryData, offset_level),
+                        character_time: readInt(this.entryData, offset3_timestamp),
+                        character_time_format: formatTime(readInt(this.entryData, offset3_timestamp))
+                    });
+                }
             }
         }
-
-
 
         return chars;
 
@@ -158,16 +185,14 @@ function readInt(buffer: Buffer, offset: number) {
     return buffer.readInt32LE(offset);
 }
 
-async function loadSL2(filePath: string, profile: Profile, encrypted: boolean) {
-
-
+async function loadSL2(filePath: string, profile: Profile) {
 
     const raw = fs.readFileSync(filePath);
-    const entry = new BND4Entry(raw, 10, profile, true);
+    const entry = new BND4Entry(raw, profile);
     const characters = entry.getCharacters();
 
     characters.forEach(e => {
-       // console.log(e);
+        console.log(e);
     });
 }
 
@@ -177,4 +202,4 @@ const filePath = [
     "C:\\Users\\guilh\\AppData\\Roaming\\DarkSoulsIII\\011000013e20cb97\\DS30000.sl2" // Encrypted DS3
 ];
 
-loadSL2(filePath[2], "ds3", true);
+loadSL2(filePath[2], "ds3");
